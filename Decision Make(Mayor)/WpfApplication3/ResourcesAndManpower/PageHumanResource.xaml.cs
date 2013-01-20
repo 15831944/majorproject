9bfc2132b66d418096c6e4570dbffbaf35320d61
formarkandroid <formarkandroid@43a5053f-994e-27dd-f31c-89f782350a12>
2013-01-20 11:22:27 +0000

    更改了人力资源描述

--- Decision Make(Mayor)/WpfApplication3/ResourcesAndManpower/PageHumanResource.xaml.cs
+++ Decision Make(Mayor)/WpfApplication3/ResourcesAndManpower/PageHumanResource.xaml.cs
@@ -57,8 +57,37 @@ namespace WpfApplication3.ResourcesAndManpower
         private void Depart_Click(object sender, RoutedEventArgs e)
         {
             string content = (sender as Button).Content.ToString();
-            BriefIntro.Text = "简介测试" + content;
-            Address.Text = "联系方式测试" + content;
+            switch (content)
+            {
+                case "办公室":
+                    BriefIntro.Text = "简介：办公室\r\n下设政策研究室、外事办，主要负责土地规划局的日常运转，同时，研究上级政策，部署本地工作，并且负责对外交流。";
+                    Address.Text = "联系方式：北京西路99号　|　邮政编码：200003 | 联系电话：021- 6319 3188 转 01";
+                    break;
+                case "组织人事处":
+                    BriefIntro.Text = "简介：组织人事处\r\n负责日常组织工作 人事事务。";
+                    Address.Text = "联系方式：北京西路99号　|　邮政编码：200003 | 联系电话：021- 6319 3188 转 02";
+                    break;
+                case "规划管理处":
+                    BriefIntro.Text = "简介：规划管理处\r\n负责土地规划，周边配套规划等。";
+                    Address.Text = "联系方式：北京西路99号　|　邮政编码：200003 | 联系电话：021- 6319 3188 转 03";
+                    break;
+                case "建筑规划管理处":
+                    BriefIntro.Text = "简介：建筑规划管理处\r\n负责土地内的建筑的设计审核，规划。";
+                    Address.Text = "联系方式：北京西路99号　|　邮政编码：200003 | 联系电话：021- 6319 3188 转 04";
+                    break;
+                case "市政规划管理处":
+                    BriefIntro.Text = "简介：市政规划管理处\r\n负责地块内的市政规划。";
+                    Address.Text = "联系方式：北京西路99号　|　邮政编码：200003 | 联系电话：021- 6319 3188 转 05";
+                    break;
+                case "综合监督管理处":
+                    BriefIntro.Text = "简介：综合监督管理处\r\n负责对政策落实及违法情况的监督，管理。";
+                    Address.Text = "联系方式：北京西路99号　|　邮政编码：200003 | 联系电话：021- 6319 3188 转 06";
+                    break;
+                case "土地综合计划处":
+                    BriefIntro.Text = "简介：土地综合计划处\r\n负责对土地的评估等。";
+                    Address.Text = "联系方式：北京西路99号　|　邮政编码：200003 | 联系电话：021- 6319 3188 转 07";
+                    break;
+            }
         }
 
         private void JuZhang_Click(object sender, RoutedEventArgs e)
